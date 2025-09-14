a0c4f4e982e400c6e1d79db6cbf1bc8ede489d23
Ashraf Inamdar <ashraf.inamdar@optiva.com>
2025-09-14 11:40:24 +0530

    web: bootstrap react-router with two routes

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,10 +1,26 @@
-import { Button } from "antd";
+import { Routes, Route, Link } from "react-router-dom";
+
+function Dashboard() {
+  return <div style={{ padding: 24 }}><h1>Dashboard</h1></div>;
+}
+
+function Incidents() {
+  return <div style={{ padding: 24 }}><h1>Incidents</h1></div>;
+}
 
 export default function App() {
   return (
-    <div style={{ padding: 24 }}>
-      <h1>alertd UI</h1>
-      <Button type="primary">AntD OK</Button>
-    </div>
+    <>
+      {/* temporary links just to test routing; we'll replace with AntD Menu next */}
+      <nav style={{ padding: 12 }}>
+        <Link to="/" style={{ marginRight: 12 }}>Home</Link>
+        <Link to="/incidents">Incidents</Link>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/incidents" element={<Incidents />} />
+      </Routes>
+    </>
   );
 }
